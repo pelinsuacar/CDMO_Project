9542de1afc274272769fc8940c54b20676e5a030
pelinsuacar <118550250+pelinsuacar@users.noreply.github.com>
2023-12-05 14:37:13 +0200

    Add files via upload

---
+++ CP/run_CP_instance.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Check if the correct number of arguments is provided
+if [ "$#" -ne 4 ]; then
+    echo "Usage: ./script.sh SOLVER MODEL OUTPUT_FILE INSTANCE_NAME"
+    exit 1
+fi
+
+TIME_LIMIT=300000
+
+# Capture the arguments
+SOLVER="$1"
+MODEL="$2"
+OUTPUT_FILE="$3"
+INSTANCE_NAME="$4"
+
+# Specify the full path to the instance
+INSTANCE_PATH="/Instances/$INSTANCE_NAME.dzn"
+MODEL_PATH="/Models/$MODEL.mzn"
+
+# Empty the output file if it exists
+> "$OUTPUT_FILE"
+
+echo "Solver: $SOLVER" >> "$OUTPUT_FILE"
+echo "Model: $MODEL" >> "$OUTPUT_FILE"
+
+echo "" >> "$OUTPUT_FILE"
+
+# Extract the instance name without the extension
+instance_name_no_ext="${INSTANCE_NAME%.dzn}"
+
+# Print the instance name to the output file
+echo "Running $instance_name_no_ext" >> "$OUTPUT_FILE"
+
+# Print the command being executed
+echo "Running command:"
+echo "minizinc --solver $SOLVER --time-limit $TIME_LIMIT --output-time $MODEL_PATH $INSTANCE_PATH"
+
+# Run the minizinc command for the specified instance and save the output to the txt file
+minizinc --solver $SOLVER --time-limit $TIME_LIMIT \
+         --output-time $MODEL_PATH $INSTANCE_PATH >> "$OUTPUT_FILE"
+
+echo "" >> "$OUTPUT_FILE"
